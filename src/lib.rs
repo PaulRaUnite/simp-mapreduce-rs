d74f6600db63494b3edc338ffc8ec36e51395c46
Pavlo Tokariev <pavlo.tokariev@gmail.com>
2020-06-26 19:01:23 +0300

    Hooks and docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-
 use rayon::prelude::*;
 use std::collections::{BTreeMap, HashMap};
 use std::hash::Hash;
-
 
 #[cfg(test)]
 mod tests {
@@ -10,21 +8,27 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(map_reduce(vec![1,2,3], |x| (1, x), |x,y| x+y).into_iter().collect::<Vec<_>>(), vec![(1,6)]);
+        assert_eq!(
+            map_reduce(vec![1, 2, 3], |x| (1, x), |x, y| x + y)
+                .into_iter()
+                .collect::<Vec<_>>(),
+            vec![(1, 6)]
+        );
     }
 }
 
-fn map_reduce<I, K, V, M, R>(
+/// Glue code for parallel local map-reduce computation.
+pub fn map_reduce<I, K, V, M, R>(
     data: impl IntoParallelIterator<Item = I>,
     map: M,
     reduce: R,
 ) -> impl IntoIterator<Item = (K, V)>
-    where
-        M: Fn(I) -> (K, V) + Sync + Send,
-        R: Fn(V, V) -> V + Sync + Send + Copy,
-        I: Sync + Send,
-        K: Sync + Send + Ord + Hash,
-        V: Sync + Send + Default,
+where
+    M: Fn(I) -> (K, V) + Sync + Send,
+    R: Fn(V, V) -> V + Sync + Send + Copy,
+    I: Sync + Send,
+    K: Sync + Send + Ord + Hash,
+    V: Sync + Send + Default,
 {
     let data: Vec<(K, V)> = data.into_par_iter().map(map).collect();
 
